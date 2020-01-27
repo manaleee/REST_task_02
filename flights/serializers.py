9eea779f7c691150a82cf87ff8476590f01b7f11
Manal Alkandari <manal_alkandri@hotmail.com>
2020-01-27 18:41:31 +0300

    task02_API

--- flights/serializers.py
+++ flights/serializers.py
@@ -3,13 +3,40 @@ from rest_framework import serializers
 from .models import Flight, Booking
 
 
+
+
+
+
 class FlightSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Flight
 		fields = ['destination', 'time', 'price', 'id']
 
 
+
+
+
+
 class BookingSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Booking
 		fields = ['flight', 'date', 'id']
+
+
+
+class BookingsDetailSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Booking
+		fields = ["flight", "date", "id", "passengers"]
+
+
+
+
+
+class BookingsUpdateSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Booking
+		fields = ["flight", "date", "id", "passengers"]
+
+
+
